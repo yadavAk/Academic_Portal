be3ed3308de37905afa4517de4c88665b3107c93
Abhishek Yadav <abhi.cs.0217@gmail.com>
2019-11-21 13:55:07 +0530

    Initial commit

---
+++ acadportal/__init__.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from acadportal.config import Config
+
+# export FLASK_APP=acadportal.py
+# export FLASK_DEBUG=1
+# flask run
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    from acadportal.users.routes import users
+    from acadportal.posts.routes import posts
+    from acadportal.main.routes import main
+    from acadportal.errors.handlers import errors
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
+
